45d3515ab6297b5f59e19022effd0ab7356b713f
dlhwang <2500134@partner.skcc.com>
2026-04-28 16:06:16 +0900

    first commit

---
+++ src/main/kotlin/io/goahead/tuk/GoaheadTukApplication.kt
@@ -0,0 +1,11 @@
+package io.goahead.tuk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoaheadTukApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoaheadTukApplication>(*args)
+}
---
+++ src/test/kotlin/io/goahead/tuk/GoaheadTukApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.goahead.tuk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoaheadTukApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
